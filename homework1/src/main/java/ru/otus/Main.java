efd5946aeb903ec7bfff2563e674c7e32a9e8d8b
alinayu <alina-yu@mail.ru>
2018-12-15 02:42:56 +0300

    hw2 :: annotaion-based config, i18n

--- homework1/src/main/java/ru/otus/Main.java
+++ homework1/src/main/java/ru/otus/Main.java
@@ -1,15 +1,29 @@
 package ru.otus;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.otus.service.TestService;
 
 /**
  * Created by alina on 09.12.2018.
  */
+@Configuration
+@ComponentScan
+@PropertySource("classpath:/application.properties")
 public class Main {
 
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasename("/i18n/bundle");
+        ms.setDefaultEncoding("UTF-8");
+        ms.setFallbackToSystemLocale(false);
+        return ms;
+    }
+
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
 
         TestService testService = context.getBean(TestService.class);
         testService.doTest(System.in, System.out);
--- homework1/src/main/java/ru/otus/service/TestServiceImpl.java
+++ homework1/src/main/java/ru/otus/service/TestServiceImpl.java
@@ -1,46 +1,75 @@
 package ru.otus.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
 import ru.otus.model.Question;
-import ru.otus.utils.InputOutputUtils;
+import sun.util.locale.LocaleUtils;
 
 import java.io.*;
 import java.util.List;
+import java.util.Locale;
+
+import static java.lang.String.valueOf;
+import static ru.otus.utils.InputOutputHelper.*;
 
 /**
  * Created by alina on 09.12.2018.
  */
+@Service
 public class TestServiceImpl implements TestService  {
 
     private QuestionService questionService;
 
     private AssessmentService assessmentService;
 
-    private InputOutputUtils inputOutputUtils;
+    private MessageSource messageSource;
 
+    @Value("${questions.csv.file.name}")
     private String questionsCsvFileName;
 
-    public TestServiceImpl(QuestionService questionService, AssessmentService assessmentService,
-                           InputOutputUtils inputOutputUtils, String questionsCsvFileName) {
+    @Value("${locale.language:ru}")
+    private String localeLanguage;
+
+    @Value("${locale.country:RU}")
+    private String localeCountry;
+
+    @Autowired
+    public TestServiceImpl(QuestionService questionService, AssessmentService assessmentService, MessageSource messageSource) {
         this.questionService = questionService;
         this.assessmentService = assessmentService;
-        this.inputOutputUtils = inputOutputUtils;
+        this.messageSource = messageSource;
+    }
+
+    public void setQuestionsCsvFileName(String questionsCsvFileName) {
         this.questionsCsvFileName = questionsCsvFileName;
+    }
+
+    public void setLocaleLanguage(String localeLanguage) {
+        this.localeLanguage = localeLanguage;
+    }
+
+    public void setLocaleCountry(String localeCountry) {
+        this.localeCountry = localeCountry;
     }
 
     public void doTest(InputStream in, OutputStream out) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
         OutputStreamWriter writer = new OutputStreamWriter(out);
+        Locale locale = new Locale(localeLanguage, localeCountry);
 
-        inputOutputUtils.writeToOutput(writer, "Представьтесь, пожалуйста: \n");
-        inputOutputUtils.readlineFromInput(reader);
+        writeToOutput(writer, messageSource.getMessage("test.greetings", null, locale));
+        readlineFromInput(reader);
 
         List<Question> questions = questionService.getQuestionsFromCsvFile(questionsCsvFileName);
 
-
-        inputOutputUtils.writeToOutput(writer, "Количество правильных ответов : " +
-                assessmentService.rate(reader, writer, questions));
-        inputOutputUtils.close(reader);
-        inputOutputUtils.close(writer);
+        writeToOutput(writer, messageSource.getMessage("test.result",
+                new String[] { valueOf(assessmentService.rate(reader, writer, questions)) },
+                locale));
+        close(reader);
+        close(writer);
     }
 
 
