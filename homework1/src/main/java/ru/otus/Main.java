147f2b4c1581a09bde45317b37b5ec50cac03710
alinayu <alina-yu@mail.ru>
2018-12-17 09:18:40 +0300

    hw3 :: to spring-boot, add banner

--- homework1/src/main/java/ru/otus/Main.java
+++ homework1/src/main/java/ru/otus/Main.java
@@ -1,32 +1,28 @@
 package ru.otus;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import ru.otus.service.TestService;
 
 /**
  * Created by alina on 09.12.2018.
  */
-@Configuration
-@ComponentScan
-@PropertySource("classpath:/application.properties")
+@SpringBootApplication
 public class Main {
 
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("/i18n/bundle");
+        ms.setBasename("classpath:/i18n/bundle");
         ms.setDefaultEncoding("UTF-8");
         ms.setFallbackToSystemLocale(false);
         return ms;
     }
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-
-        TestService testService = context.getBean(TestService.class);
-        testService.doTest(System.in, System.out);
+        SpringApplication.run(Main.class, args);
     }
 }
 
