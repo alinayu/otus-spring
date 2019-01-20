9cc8519b051c15884cc907dbf7eb7a5f4113af6f
alinayu <alina-yu@mail.ru>
2019-01-20 13:59:45 +0300

    hw6 :: jpa

--- homework2-books/src/main/java/ru/otus/Main.java
+++ homework2-books/src/main/java/ru/otus/Main.java
@@ -1,15 +1,11 @@
 package ru.otus;
 
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLException;
-
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
         SpringApplication.run(Main.class);
-        Console.main(args);
     }
 }
